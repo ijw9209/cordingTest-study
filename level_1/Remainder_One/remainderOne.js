be14fb827fe518679a1d5fc14a3990eebd374b94
jae woong <ijw9209@naver.com>
2022-10-19 15:29:05 +0900

    나머지가 1이 되는수

--- level_1/Remainder_One/remainderOne.js
+++ level_1/Remainder_One/remainderOne.js
@@ -2,19 +2,18 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/87389
 
 remainderOne = (n) => {
-    //나머지가 1이되는 수 배열
-    const arr = [];
-    //인덱싱 나눠서 1이되는 수가 존재할때만 증가
-    let idx = 0;
+    //최소 값을 담을 변수    
+    let min = 0;
+        
     for(let x = 0; x < n; x++){
         
         if(n % x === 1) {
-            arr[idx] = x;
-                idx++;
+            min = x;
+            break;
         }
     }
     
     //가장 작은 수
-    return Math.min(...arr);
+    return min;
 }
 module.exports = remainderOne;
