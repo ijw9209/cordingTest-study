ffd3f005a119c962901146e8277e095550471da0
이재웅 <ijw9209@naver.com>
2022-12-10 01:55:33 +0900

    test update

--- level_1/nearString/nearString.test.js
+++ level_1/nearString/nearString.test.js
@@ -1,11 +1,11 @@
 const nearString = require('./nearString');
 
-test('최소직사각형 1번 Case ', () => {
+test('가장 가까운 같은 글자 1번 Case ', () => {
   expect(nearString("banana")).toBe([-1, -1, -1, 2, 2, 2]);
 });
 
 
-test('최소직사각형 2번 Case ', () => {
+test('가장 가까운 같은 글자 2번 Case ', () => {
   expect(nearString("foobar")).toBe([-1, -1, 1, -1, -1, -1]);
 });
 
