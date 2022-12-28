73374d8d50ceb01cc834646b214650a13145cde6
jae woong <ijw9209@naver.com>
2022-12-28 17:33:43 +0900

    숫자짝궁 풀이 완료

--- level_1/numberpartner/numberpartner.js
+++ level_1/numberpartner/numberpartner.js
@@ -3,34 +3,67 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/131128
 
 numberpartner = (X, Y) => {
+   
     var answer = '';
     
-    let sameArr = [];
-    let idx = 0;
-    let xArr = X;
-    let yStr = Y;
+    let xArr = X.split('').sort();
+    let yArr = Y.split('').sort();
     
-    for(let i = 0; i < xArr.length; i++) {
+    for(let i = 0; i <= 9; i++){
+        let xCount = X.split(i).length - 1;
+        let yCount = Y.split(i).length - 1;
+        xArr[i] = xCount;
+        yArr[i] = yCount;
         
-        if(yStr.indexOf(xArr[i]) === -1){
-            
-        }else {
-            let sameIdx = yStr.indexOf(xArr[i]);
-            answer += yStr[sameIdx] + '';
-            // if(answer === "00") {
-            //     answer = "0";
-            // }
-            idx++;
-            yStr = yStr.replace(yStr[sameIdx] , '');
-            // console.log(yStr);
+    }
+    
+    let sameCnt = 0;
+    
+    for(let j = 9; j >= 0; j--) {
+        
+        if(xArr[j] > 0 && yArr[j] > 0) {
+            cnt = Math.min(xArr[j], yArr[j]);
+            sameCnt++;
+            while(cnt > 0){
+                
+                answer += j;
+                cnt--;
+            }
         }
     }
     
-    // let n_answer = Number(sameArr.sort().reverse().join(''));
-    // answer.sort(function(a, b) { return b - a});
-    // console.log(answer);
-    // return idx === 0 ? "-1" : String(n_answer);
-    return idx === 0 ? "-1" : answer.split('').sort().reverse().join('');
+    if(sameCnt === 0) {
+      answer = "-1";   
+    }
+    
+    
+    return (answer[0] === "0") ? "0" : answer;
+   
+    // ===============이전풀이 시간복잡도 탈락 =========////////
+   
+    // var answer = '';
+    
+    // let sameArr = [];
+    // let idx = 0;
+    // let xArr = X;
+    // let yStr = Y;
+    
+    // for(let i = 0; i < xArr.length; i++) {
+        
+    //     if(yStr.indexOf(xArr[i]) === -1){
+            
+    //     }else {
+    //         let sameIdx = yStr.indexOf(xArr[i]);
+    //         answer += yStr[sameIdx] + '';
+    //         // if(answer === "00") {
+    //         //     answer = "0";
+    //         // }
+    //         idx++;
+    //         yStr = yStr.replace(yStr[sameIdx] , '');
+    //     }
+    // }
+    
+    // return idx === 0 ? "-1" : answer.split('').sort().reverse().join('');
 }
 
 module.exports = numberpartner;
